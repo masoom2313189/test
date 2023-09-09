c5e15f318ef929c666c2a559eb6bc898266f8aeb
Masoom Rizvi <masoom2313189@gmail.com>
2023-09-09 17:33:48 +0530

    adding

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
-const url = 'mongodb+srv://shiva850681:Kaliya8506@cluster0.cro6kwk.mongodb.net/Issue_tracker?retryWrites=true&w=majority';
-// mongoose.connect('url') 
-mongoose.connect(url);
-const db=mongoose.connection;
+const mongoose = require("mongoose");
+// mongoose.connect('url')
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
 
 //If any Error then Getting this Line
-db.on('error',console.error.bind(console,"Error connecting to MongoDB"));   
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-
-db.once('open',()=>{
-    console.log("Connected to Database :: MongoDB ")
+db.once("open", () => {
+  console.log("Connected to Database :: MongoDB ");
 });
 
-module.exports=db;  //Exports db
+module.exports = db; //Exports db
