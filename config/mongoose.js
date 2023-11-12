2fc9f623d4740b322748681f87ae79a847187e86
Masoom Rizvi <105916377+masoom2313189@users.noreply.github.com>
2023-11-12 17:29:40 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // connecting mongoose to its default server and ecommerceDB
-mongoose.connect('mongodb+srv://soumyasri2245:Soumya22%4034@cluster0.u2ywt3o.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://krishnaurkude38:ydSMGU3AA4RpaugX@cluster0.vppzteh.mongodb.net/', {
     useNewUrlParser: true
 });
 
